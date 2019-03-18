ecf348f38effa1ff31b0410e1cba6bd28f7c889f
Your Name <you@example.com>
2019-03-18 21:01:03 +0800

    709. 转换成小写字母

--- src/easy/toLowerCase.py
+++ src/easy/toLowerCase.py
@@ -9,17 +9,23 @@
 """
 这题难道有什么特别的技巧，很难上80%
 """
+import time
+
+
 class Solution:
     def toLowerCase(self, str: str) -> str:
+        start = time.time()
         result = ''
         ascii_A = ord('A')
         asscii_a = ord('a')
         for char in str:
             if char >= 'A' and char <= 'Z':
-                result += chr(ord(char) + asscii_a - ascii_A)
+                # result += chr(ord(char) + asscii_a - ascii_A)
+                result += chr(ord(char) | 32)
             else:
                 result += char
         # return str(string).lower()
+        print(time.time() - start)
         return result
 
 
