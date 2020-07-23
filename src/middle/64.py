00ac387ddf0a46e8b6694430a7f8c85c43fc7887
liu wei <you@example.com>
2020-07-23 08:31:56 +0800

    64

--- src/middle/64.py
+++ src/middle/64.py
@@ -1,35 +1,53 @@
 """
-求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+64. 最小路径和
+给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
 
- 
+说明：每次只能向下或者向右移动一步。
 
-示例 1：
+示例:
 
-输入: n = 3
-输出: 6
-示例 2：
-
-输入: n = 9
-输出: 45
- 
-
-限制：
-
-1 <= n <= 10000
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/qiu-12n-lcof
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+输入:
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+输出: 7
+解释: 因为路径 1→3→1→1→1 的总和最小。
 """
+from typing import List
 
 
 class Solution:
-    def sumNums(self, n: int) -> int:
-        return n and (n + self.sumNums(n - 1))
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        if len(grid) == 0 or len(grid[0]) == 0:
+            return 0
+
+        n = len(grid)
+        m = len(grid[0])
+        dp = []
+        for i in range(0, n):
+            dp.append([0] * m)
+        dp[0][0] = grid[0][0]
+
+        for i in range(0, n):
+            for j in range(0, m):
+                if i == 0 and j == 0:
+                    continue
+                elif i == 0:
+                    dp[i][j] = dp[i][j-1] + grid[i][j]
+                elif j == 0:
+                    dp[i][j] = dp[i-1][j] + grid[i][j]
+                else:
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+        return dp[n-1][m-1]
 
 
 if __name__ == "__main__":
     s = Solution()
-
-    assert s.sumNums(3) == 6
-    assert s.sumNums(9) == 45
+    grid = [
+        [1, 3, 1],
+        [1, 5, 1],
+        [4, 2, 1]
+    ]
+    print(s.minPathSum(grid))
