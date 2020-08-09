2807af92b7ff7fa82ad0f9126b44f58665061599
liu wei <you@example.com>
2020-08-09 20:51:54 +0800

    99,105,93,77,46,47,236

---
+++ src/middle/99.py
@@ -0,0 +1,66 @@
+"""
+99. 恢复二叉搜索树
+二叉搜索树中的两个节点被错误地交换。
+
+请在不改变其结构的情况下，恢复这棵树。
+
+示例 1:
+
+输入: [1,3,null,null,2]
+
+   1
+  /
+ 3
+  \
+   2
+
+输出: [3,1,null,null,2]
+
+   3
+  /
+ 1
+  \
+   2
+示例 2:
+
+输入: [3,1,4,null,null,2]
+
+  3
+ / \
+1   4
+   /
+  2
+
+输出: [2,1,4,null,null,3]
+
+  2
+ / \
+1   4
+   /
+  3
+进阶:
+
+使用 O(n) 空间复杂度的解法很容易实现。
+你能想出一个只使用常数空间的解决方案吗？
+通过次数29,923提交次数49,480
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def recoverTree(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        Trees = lambda x: [] if not x else Trees(x.left) + [x] + Trees(x.right)
+        a = Trees(root)
+        sa = sorted(a, key=lambda x: x.val)
+        tmp = [a[i] for i in range(len(a)) if a[i] != sa[i]]
+        tmp[0].val, tmp[1].val = tmp[1].val, tmp[0].val
