31b6c5a454aff9aec54722e38f89f9d46b476ad7
lw1243925457 <qq1243925457@gmail.com>
2020-10-06 10:33:59 +0800

    刷题

---
+++ leetcode/editor/cn/[240]搜索二维矩阵 II.py
@@ -0,0 +1,37 @@
+# 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target。该矩阵具有以下特性： 
+# 
+#  
+#  每行的元素从左到右升序排列。 
+#  每列的元素从上到下升序排列。 
+#  
+# 
+#  示例: 
+# 
+#  现有矩阵 matrix 如下： 
+# 
+#  [
+#   [1,   4,  7, 11, 15],
+#   [2,   5,  8, 12, 19],
+#   [3,   6,  9, 16, 22],
+#   [10, 13, 14, 17, 24],
+#   [18, 21, 23, 26, 30]
+# ]
+#  
+# 
+#  给定 target = 5，返回 true。 
+# 
+#  给定 target = 20，返回 false。 
+#  Related Topics 二分查找 分治算法 
+#  👍 444 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        
+# leetcode submit region end(Prohibit modification and deletion)
